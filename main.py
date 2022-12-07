77e7c2516f74a28a049d458c6dfc7a4164ff9d5e
arjun-234 <arjun.darji@yudiz.com>
2022-12-07 11:02:34 +0530

    impelemented auto-refresher in git

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import git 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return "Hello From FLask"
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 if __name__ == "__main__":
 	app.run()
