89bcf7a318c6bf2fb6fa6e48cc0f0393acf84cef
arjun-234 <arjun.darji@yudiz.com>
2022-12-16 13:01:14 +0530

    update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello From FLask gautam"
+	return "Hello From CI/CD"
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
